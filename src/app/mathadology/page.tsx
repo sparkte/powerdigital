d9afc210723e9b1395fd69edd0760e408b9716af
sparkte <newmobi666@gnail.com>
2025-10-28 02:58:27 +0500

    commit

--- src/app/mathadology/page.tsx
+++ src/app/mathadology/page.tsx
@@ -1,6 +1,6 @@
-// import Mainnova from '../../components/methadology/main' 
+import Mainnova from '../../components/b2b/mainb2b' 
 
-// export default function nova() {
-//   return <Mainnova />;
-// }
+export default function nova() {
+  return <Mainnova />;
+}
 
